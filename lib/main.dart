e4df3e02def80265f8b58f253de8415a90945fed
yara30999 <yaranaserelniny@gmail.com>
2024-12-24 09:59:01 +0200

    initialize easy localization package

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'resources/routes_manager.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+  runApp(
+    EasyLocalization(
+        supportedLocales: [Locale('en', 'US'), Locale('ar', 'EG')],
+        path: 'assets/translations',
+        fallbackLocale: Locale('en', 'US'),
+        child: MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -13,6 +23,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Cosmetics App Demo',
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       initialRoute: Routes.loginRoute,
       onGenerateRoute: RouteGenerator.getRoute,
     );
