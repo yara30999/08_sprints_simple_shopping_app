d85d16c4774e77abc969ff44ba93b229ea7d860e
yara30999 <yaranaserelniny@gmail.com>
2025-01-28 18:51:24 +0200

    add firebase to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 import 'resources/routes_manager.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(
     EasyLocalization(
